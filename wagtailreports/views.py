b7231edb13cbc6d9a4960ca4d57e99bd47895324
Damon <damon@vix.digital>
2017-10-20 12:24:52 +0100

    merged in template logic

--- wagtailreports/views.py
+++ wagtailreports/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import redirect, render
+from wagtail.wagtailcore.models import Page, PageRevision
 
 def reports(request):
-    return render(request, 'reports/dashboard.html', {
-    })
 
-def settings(request):
-    return render(request, 'reports/settings.html', {
+    def get_live_pages():
+        live_pages = Page.objects.in_site(request.site).live()
+        return live_pages
+
+    def get_page_results():
+        page_results = get_live_pages()
+        order_by = request.GET.get('orderby')
+
+        if order_by:
+            page_results = page_results.order_by(order_by)
+        else:
+            # default to showing most stale content first
+            page_results = page_results.order_by('last_published_at')
+
+        return page_results
+
+    return render(request, 'reports/dashboard.html', {
+        'page_results': get_page_results
     })
